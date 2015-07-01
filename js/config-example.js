9ca6b22b67a7f801f360331b53068ca06574b2d9
iCarto Team <info@icarto.es>
2015-07-01 11:15:50 +0200

    add uri in config-example

--- js/config-example.js
+++ js/config-example.js
@@ -1,4 +1,5 @@
 var cdb_account = 'cartodb username',
     cdb_properties_table = 'cartodb properties table name',
     cdb_projects_table = 'projects table name',
+    cdb_inventory_viz = 'inventory uri',
     ccsi_api_project = 'url to add project';
